aaf67cb3d742ed1f33c63b5a2b91b55428afb4ed
Nhi Nguyen <nhintnguyenai@gmail.com>
2025-11-12 20:50:22 +0100

    Created the app store project

---
+++ app_store/src/main/java/com/app_store/AppStoreApplication.java
@@ -0,0 +1,14 @@
+package com.app_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppStoreApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
